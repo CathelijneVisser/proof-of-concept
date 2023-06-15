9ed1c1683cc19126075f285f0985ffd9e7d40266
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-06-15 10:28:03 +0200

    wip

--- server.js
+++ server.js
@@ -16,28 +16,62 @@ app.use(express.urlencoded({ extended: true }))
 // Serve static files from the public directory
 app.use(express.static("public"))
 
+const sportiek =
+  "https://raw.githubusercontent.com/DikkeTimo/proof-of-concept-Sportiek/main/json/localjssportiek.json";
+
+const datasportiek = [[sportiek]];
+const [data1] = await Promise.all(datasportiek.map(fetchJson));
+const data = { data1 };
+
+// app.get("/", async function (request, response) {
+
+//   fetchJson(`${process.env.sportiek}`).then((data) => {
+//     const allIds = [...new Set(data.map(item => item.accomodationId))]
+
+//     const accomodations = []
+//     allIds.forEach(id => {
+//       const rows = data.filter(item => {
+//         return item.accomodationId === id
+//       })
+//       if (rows.length) {
+//         accomodations[id] = rows
+//       }
+//     })
+
+
+//     response.render("index", { data, complex: accomodations })
+//   })
+// })
+
+
+const filterData = data1.reduce((acc, item) => {
+  const existingItem = acc.find((el) => el.variantName === item.variantName);
+
+  if (existingItem) {
+    // Voeg de huidige datum alleen toe als deze nog niet in de bestaande item is opgenomen
+    if (!existingItem.departureDates.includes(item.departureDate)) {
+      existingItem.departureDates.push(item.departureDate);
+    }
+  } else {
+    // Voeg een nieuw item toe met de huidige variantName en datum
+    acc.push({
+      accomodationId: item.accomodationId,
+      variantName: item.variantName,
+      complex_name: item.complex_name,
+      departureDates: [item.departureDate]
+    });
+  }
+
+  return acc;
+}, []);
+
 app.get("/", async function (request, response) {
+  response.render("index", { data1: data1, filterData: filterData });
+});
 
-  fetchJson(`${process.env.sportiek}`).then((data) => {
-    const allIds = [...new Set(data.map(item => item.accomodationId))]
-
-    const accomodations = []
-    allIds.forEach(id => {
-      const rows = data.filter(item => {
-        return item.accomodationId === id;
-      })
-      if (rows.length) {
-        accomodations[id] = rows
-      }
-    })
-
-
-    response.render("index", { data, accomodations: accomodations })
-  })
-})
 
 //poortnummer instellen
-app.set("port", 8000)
+app.set("port", 5000)
 //start de server
 app.listen(app.get("port"), () => {
   console.log(`Application started on http://localhost:${app.get("port")}`)
